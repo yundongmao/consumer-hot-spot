1f759282cefbf7ea7506aa0e9c755171e3062750
yinizhizhu <lijiahe19941013@sina.cn>
2015-05-03 20:33:48 +0800

    to add the tf-idf#

---
+++ tf-idf/include/tfidf.h
@@ -0,0 +1,37 @@
+﻿#ifndef TFIDF_H
+#define TFIDF_H
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include "../cppjieba/dict/idf.utf8"
+
+using namespace std;
+
+const int sonunm = 1920, base = 0x80;
+
+
+struct TF
+{
+	char data;
+	double grade;
+	bool terminal;
+	struct TF *son[sonnum];
+	TF()
+	{
+		data ='\0', grade = 0;
+		terminal = false; 
+		memset(son, NULL, sizeof(son));
+	}
+}
+
+class grade
+{
+public:
+	
+private:
+	TF* NewTF();
+	void Insert(TF *root, char *s, double tmp);
+	void DeleteAll(TF *root);
+	bool DeleteWord(TF *root, char *word);
+}
---
+++ tf-idf/tfidf.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+#include "include/tfidf.h"
+
+using namespace std;
+
+TF* grade::NewTF()
+{
+	return;
+}
+void grade::Insert(TF *root, char *s, double tmp)
+{
+	return;
+}
+void grade::DeleteAll(TF *root)
+{
+	return;
+}
+bool grade::DeleteWord(TF *root, char *word)
+{
+	return;
+}
